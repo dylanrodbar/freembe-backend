eabebab690e14c3af15598dfd241afcc46b6a084
dylanrodbar <dylanrodbar97@gmail.com>
2018-05-30 02:18:38 -0600

    crud categorias

--- freembe-backend/db/schema.rb
+++ freembe-backend/db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530043830) do
+ActiveRecord::Schema.define(version: 20180530064636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "photo"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "subcategories", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.string   "photo"
