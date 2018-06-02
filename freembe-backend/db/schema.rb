e7c2a040dd39b4a2a0b7ddeb3489fc2fdcb21587
dylanrodbar <dylanrodbar97@gmail.com>
2018-06-02 02:20:11 -0600

    modelo de usuario creado

--- freembe-backend/db/schema.rb
+++ freembe-backend/db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602070710) do
+ActiveRecord::Schema.define(version: 20180602081459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.float    "price"
+    t.string   "photo"
+    t.integer  "author"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.integer  "subcategory_id"
+    t.string   "place"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
